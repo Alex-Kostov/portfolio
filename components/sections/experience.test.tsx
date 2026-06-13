c46add0a6602198415f9d42490af5ad0c02d574a
Aleksandar Kostov <alex89773@gmail.com>
2026-06-13 20:55:57 +0300

    Add experience section

---
+++ components/sections/experience.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { Experience } from "./experience";
+
+describe("Experience", () => {
+  it("renders each company", () => {
+    render(<Experience />);
+    expect(screen.getByText("Nexo")).toBeInTheDocument();
+    expect(screen.getByText("Pateplay")).toBeInTheDocument();
+    expect(screen.getByText("DevriX")).toBeInTheDocument();
+  });
+});
